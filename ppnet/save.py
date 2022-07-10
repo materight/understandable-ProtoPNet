d3c1e711c5b14789e96a2cc2759e9aa1f9cc121e
materight <matteo.est@gmail.com>
2022-07-10 20:50:11 +0200

    Fix

--- ppnet/save.py
+++ ppnet/save.py
@@ -1,6 +1,7 @@
 import os
 import torch
 
+from .helpers import makedir
 
 def save_model_w_condition(model, model_dir, model_name, accu, target_accu, log=print, epoch=None):
     '''
@@ -8,4 +9,5 @@ def save_model_w_condition(model, model_dir, model_name, accu, target_accu, log=
     '''
     if accu > target_accu or (epoch is not None and epoch % 50 == 0):
         log('\tabove {0:.2f}%'.format(target_accu * 100))
+        makedir(os.path.join(model_dir, 'checkpoints'))
         torch.save(obj=model, f=os.path.join(model_dir, 'checkpoints', (model_name + '{0:.4f}.pth').format(accu*100)))
