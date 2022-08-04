6381be57b70e4224219d15f0d27aba3f7f7d138b
materight <matteo.est@gmail.com>
2022-08-04 22:30:09 +0200

    Update alignment analysis with part locations

--- ppnet/alignment_analysis.py
+++ ppnet/alignment_analysis.py
@@ -4,7 +4,7 @@ import shutil
 import cv2
 import numpy as np
 import pandas as pd
-from collections import defaultdict
+import glob
 from argparse import Namespace
 from tqdm import tqdm
 import matplotlib.pyplot as plt
@@ -39,6 +39,20 @@ def alignment_score(part_locs, high_act_patch_indices):
     high_act_y, high_act_x = np.mean(high_act_patch_indices[0:2], dtype=int), np.mean(high_act_patch_indices[2:4], dtype=int)
     dist = ((part_locs['x'] - high_act_x) ** 2 + (part_locs['y'] - high_act_y) ** 2) ** .5  # Euclidean distances
     return dist
+
+
+def scale_part_locs(part_locs, img_paths, img_resize):
+    """Scale part locations to the corresponding cropped image scale."""
+    img_ids = []
+    for img_path in tqdm(img_paths):
+        img_id = int(os.path.splitext(os.path.basename(img_path))[0])
+        img_width, img_height = Image.open(img_path).size
+        part_locs.loc[part_locs.image_id == img_id, 'x'] *= img_resize / img_width
+        part_locs.loc[part_locs.image_id == img_id, 'y'] *= img_resize / img_height
+        img_ids.append(img_id)
+    part_locs = part_locs.astype({'x': int, 'y': int})
+    assert np.all(part_locs.loc[part_locs.image_id.isin(img_ids), ['x', 'y']] <= img_resize), 'Part locations are outside of image boundaries'
+    return part_locs, img_ids
 
 
 def run_analysis(args: Namespace):
@@ -79,19 +93,16 @@ def run_analysis(args: Namespace):
             transforms.Resize(size=(img_resize, img_resize)),
             transforms.ToTensor(),
         ]))
-    image_ids = [ int(os.path.splitext(os.path.basename(img_path))[0]) for img_path, _ in dataset.samples ]
-    image_sizes = [Image.open(img_path).size for img_path, _ in dataset.samples]
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=args.num_workers, pin_memory=False)
 
     # Load part locations
-    log('Loading part locations...')
+    log('Loading test split part locations...')
     part_locs = pd.read_csv(os.path.join(os.path.dirname(dataset_split_path), 'part_locs.csv'))
-    part_locs = part_locs[part_locs.image_id.isin(image_ids)]
-    for img_id, (img_width, img_height) in zip(image_ids, image_sizes):
-        part_locs.loc[part_locs.image_id == img_id, 'x'] *= img_resize / img_width
-        part_locs.loc[part_locs.image_id == img_id, 'y'] *= img_resize / img_height
-    part_locs = part_locs.astype({'x': int, 'y': int})
-    assert np.all(part_locs[['x', 'y']] <= img_resize), 'Part locations are outside of image boundaries'
+    part_locs, image_ids = scale_part_locs(part_locs, [img_path for img_path, _ in dataset.samples], img_resize)
+    
+    # Scale also training part location to visalize prototypes
+    log('Loading train split part locations...')
+    part_locs, train_img_ids = scale_part_locs(part_locs, [img_path for img_path in glob.glob(os.path.join(args.dataset, 'train', '*/*.jpg'))], img_resize)
 
     # Load prototypes info
     load_img_dir = os.path.join(os.path.dirname(args.model), '..', 'img')
@@ -120,7 +131,13 @@ def run_analysis(args: Namespace):
         class_prototypes_indices = np.argwhere(prototype_info[:, -1] == class_idx).flatten()
 
         # Save prototypes for each class
+        alignment_matrix = pd.DataFrame(0, index=class_prototypes_indices, columns=part_locs.part_name.unique())
         for i, prototype_index in enumerate(tqdm(class_prototypes_indices, desc=f'Saving learned prototypes for class "{class_name}"')):
+            image_id = train_img_ids[prototype_info[prototype_index][0]]
+            prototype_part_locs = part_locs.loc[part_locs.image_id == image_id].set_index('part_name')
+            align_score = alignment_score(prototype_part_locs, prototype_info[prototype_index][1:5]).reindex(alignment_matrix.columns)
+            alignment_matrix.loc[prototype_index, :] = align_score.fillna(np.inf)
+            closest_part = align_score.idxmin()
             save_prototype_original_img_with_bbox(
                 load_img_dir=load_img_dir,
                 fname=os.path.join(class_dir, f'{i+1}_prototype_{prototype_index}_bbox.png'),
@@ -130,8 +147,10 @@ def run_analysis(args: Namespace):
                 bbox_height_end=prototype_info[prototype_index][2],
                 bbox_width_start=prototype_info[prototype_index][3],
                 bbox_width_end=prototype_info[prototype_index][4],
-                color=(0, 255, 255)
+                color=(0, 255, 255),
+                markers=prototype_part_locs.loc[[closest_part], ['x', 'y']].values.tolist()
             )
+        save_alignment_matrix(os.path.join(class_dir, f'alignment_matrix_prototypes.png'), alignment_matrix)
 
         # Compute alignment matrix on class images
         class_image_ids = [(i, img_id) for i, img_id in enumerate(image_ids) if dataset.samples[i][1] == class_idx]
@@ -150,5 +169,5 @@ def run_analysis(args: Namespace):
         alignment_matrix /= alignment_counts
         alignment_matrix[alignment_counts == 0] = np.inf
         # Plot alignment matrix
-        save_alignment_matrix(os.path.join(class_dir, f'alignment_matrix.png'), alignment_matrix)
+        save_alignment_matrix(os.path.join(class_dir, f'alignment_matrix_samples.png'), alignment_matrix)
     logclose()
--- ppnet/global_analysis.py
+++ ppnet/global_analysis.py
@@ -18,11 +18,16 @@ from .preprocess import preprocess_input_function
 
 def save_prototype_original_img_with_bbox(load_img_dir, fname, epoch, index,
                                           bbox_height_start, bbox_height_end,
-                                          bbox_width_start, bbox_width_end, color=(0, 255, 255)):
+                                          bbox_width_start, bbox_width_end,
+                                          color=(0, 255, 255), markers=None):
     p_img_bgr = cv2.imread(os.path.join(load_img_dir, 'epoch-'+str(epoch), str(index) + '_prototype-img-original.png'))
     cv2.rectangle(p_img_bgr, (bbox_width_start, bbox_height_start), (bbox_width_end-1, bbox_height_end-1), color, thickness=2)
+    if markers is not None:
+        for marker in markers:
+            cv2.circle(p_img_bgr, (int(marker[0]), int(marker[1])), 5, (0, 0, 255), -1)
     p_img_rgb = p_img_bgr[..., ::-1]
     p_img_rgb = np.float32(p_img_rgb) / 255
+
     plt.axis('off')
     plt.imsave(fname, p_img_rgb)
 
